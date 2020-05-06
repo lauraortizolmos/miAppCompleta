86265742ace7e8827d4e043c8ba30f4129ffa38e
lauraortizolmos <laura.ortizolmos@gmail.com>
2020-05-06 02:19:02 -0500

    App

---
+++ keys.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+    database: {
+        /*connectionLimit: 10,
+        host: 'remotemysql.com',
+        user: 'fOPs1ZlZWr',
+        password: 'yy4VP1ikbV',
+        database: 'fOPs1ZlZWr'*/
+
+        connectionLimit: 10,
+        host: 'remotemysql.com',
+        user: 'VGEEse9ACf',
+        password: 'Z3DWYpkt9u',
+        database: 'VGEEse9ACf'
+    }
+
+};
